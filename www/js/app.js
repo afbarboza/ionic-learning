c35b9e775e14533ce54593705dbaa3db3d36ddd8
Alex Frederico Ramos Barboza <alex.barboza@usp.br>
2018-01-07 21:49:11 -0200

    create modules

--- www/js/app.js
+++ www/js/app.js
@@ -1,79 +1,76 @@
 (function () {
-    var app = angular.module('starter', ['ionic']);
+    var app = angular.module( 'mynotes', [ 'ionic', 'mynotes.notestore' ] );
 
+    app.config( function ( $stateProvider, $urlRouterProvider ) {
+        $stateProvider.state( 'list', {
+            url         : '/list',
+            templateUrl : 'templates/list.html'
+        } );
 
-    app.config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('list', {
-            url: '/list',
-            cache: false,
-            templateUrl: 'templates/list.html'
-        });
+        $stateProvider.state( 'edit', {
+            url         : '/edit/:noteId',
+            templateUrl : 'templates/edit.html',
+            controller  : 'EditCtrl'
+        } );
 
-        $stateProvider.state('edit', {
-            url: '/edit/:noteId',
-            cache: false,
-            templateUrl: 'templates/edit.html'
-        });
+        $stateProvider.state( 'add', {
+            url         : '/add',
+            templateUrl : 'templates/edit.html',
+            controller  : 'AddCtrl'
+        } );
 
-        $urlRouterProvider.otherwise('/list');
-    });
+        $urlRouterProvider.otherwise( '/list' );
+    } );
 
+    app.controller( 'ListCtrl', function ( $scope, NoteStore ) {
+        $scope.notes = NoteStore.list();
+    } );
 
-    var notes = [
-        {
-            id: '1',
-            title: 'First Note',
-            description: 'This is my first note'
-        },
-        {
-            id: '2',
-            title: 'Second Note',
-            description: 'This is my second note'
-        },
-        {
-            id: '3',
-            title: 'Third Note',
-            description: 'This is my third note'
-        }
-    ];
-
-    function getNote( noteId ) {
-      for (var i = 0; i < notes.length; i++) {
-        if (notes[i].id === noteId) {
-          return notes[i];
-        }
-      }
-      return undefined;
-    }
-
-    app.controller('ListCtrl', function($scope) {
-        $scope.notes = notes;
-    });
-
-    app.controller('EditCtrl', function($scope, $state) {
+    app.controller( 'EditCtrl', function ( $scope, $state, NoteStore ) {
 
         /* getting the id passed as a parameter */
         $scope.noteId = $state.params.noteId;
-        $scope.note = getNote($scope.noteId);
-    });
 
-    app.run(function($ionicPlatform) {
-        $ionicPlatform.ready(function() {
-            if(window.cordova && window.cordova.plugins.Keyboard) {
+        $scope.note = angular.copy( NoteStore.get( $scope.noteId ) );
+
+        $scope.save = function () {
+            NoteStore.update( $scope.note );
+            $state.go( 'list' );
+        }
+    } );
+
+    app.controller( 'AddCtrl', function ( $scope, $state, NoteStore ) {
+
+        /* getting the id passed as a parameter */
+        $scope.note = {
+            id          : new Date().getTime().toString(),
+            title       : '',
+            description : ''
+        }
+
+        $scope.save = function () {
+            NoteStore.create( $scope.note );
+            $state.go( 'list' );
+        }
+    } );
+
+    app.run( function ( $ionicPlatform ) {
+        $ionicPlatform.ready( function () {
+            if ( window.cordova && window.cordova.plugins.Keyboard ) {
                 // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
                 // for form inputs)
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar( true );
 
                 // Don't remove this line unless you know what you are doing. It stops the viewport
                 // from snapping when text inputs are focused. Ionic handles this internally for
                 // a much nicer keyboard experience.
-                cordova.plugins.Keyboard.disableScroll(true);
+                cordova.plugins.Keyboard.disableScroll( true );
             }
-            if(window.StatusBar) {
+            if ( window.StatusBar ) {
                 StatusBar.styleDefault();
             }
-        });
-    })
+        } );
+    } )
 }());
 
 
